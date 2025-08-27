aa09cdf94e8ba34a18247b7ca760493f5967a142
Yuri Oliveira Manhaes <yurolive@c1r13s2.42madrid.com>
2025-08-27 18:15:59 +0200

    Ex04

---
+++ MileStone_4/Cpp1/ex04/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yurolive <yurolive@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/27 18:13:25 by yurolive          #+#    #+#             */
+/*   Updated: 2025/08/27 18:13:38 by yurolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Replacer.hpp"
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: ./replace <filename> <s1> <s2>" << std::endl;
+        return 1;
+    }
+
+    std::string filename = argv[1];
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+
+    if (s1.empty()) {
+        std::cerr << "Error: s1 (string to replace) cannot be empty." << std::endl;
+        return 1;
+    }
+
+    Replacer replacer(filename, s1, s2);
+    if (!replacer.process()) {
+        std::cerr << "Error: Could not process file." << std::endl;
+        return 1;
+    }
+
+    std::cout << "File processed successfully." << std::endl;
+    return 0;
+}
