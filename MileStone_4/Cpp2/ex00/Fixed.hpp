ab405664f337a9dbd6cdbc6ab52d2247d335c9ce
Yuri Oliveira Manhaes <yurolive@c2r16s6.42madrid.com>
2025-09-18 17:27:07 +0200

    Cpp2

---
+++ MileStone_4/Cpp2/ex00/Fixed.hpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yurolive <yurolive@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/18 16:18:37 by yurolive          #+#    #+#             */
+/*   Updated: 2025/09/18 16:24:38 by yurolive         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+	private:
+		int					_value;
+		static const int	_fractionalBits = 8;
+
+	public:
+		Fixed();
+		Fixed(const Fixed& other);
+		Fixed& operator=(const Fixed& other);
+		~Fixed();
+
+		int getRawBits(void) const;
+		void setRawBits(int const raw);
+};
+
+#endif //FIXED_HPP
